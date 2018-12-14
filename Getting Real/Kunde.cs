1be2744c10ed663ddb5addbbf0aabd0c9bf6cb41
Erik Willert Hansen <erik3891@edu.eal.dk>
2018-12-14 13:29:52 +0100

    kunde

--- Getting Real/Kunde.cs
+++ Getting Real/Kunde.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using 
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Getting_Real
 {
     class Kunde
     {
+        private static string connection = "Server=eal.local; Database=C_DB11_2018; User Id=C_STUDENT11; Password=C_OPENDB11";
         public void Showkunde()
         {
             Console.WriteLine("Indtast Kunde ID");
